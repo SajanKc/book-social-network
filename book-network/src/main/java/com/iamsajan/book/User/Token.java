42a185f7c9adf21854a1ffbe48575f10266e3dbb
sajan96kc <sajan1996kc@gmail.com>
2024-06-25 23:48:39 +0545

    add: spring security config

---
+++ book-network/src/main/java/com/iamsajan/book/User/Token.java
@@ -0,0 +1,29 @@
+package com.iamsajan.book.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "userId")
+    private User user;
+}
