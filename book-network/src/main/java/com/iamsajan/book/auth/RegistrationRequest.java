ebaa07c69ea7a1e35727e2a24c4ba6021a8c3207
sajan96kc <sajan1996kc@gmail.com>
2024-06-26 16:39:17 +0545

    add: register api created

---
+++ book-network/src/main/java/com/iamsajan/book/auth/RegistrationRequest.java
@@ -0,0 +1,30 @@
+package com.iamsajan.book.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RegistrationRequest {
+    @NotEmpty(message = "First name is required")
+    @NotBlank(message = "First name is required")
+    private String firstname;
+
+    @NotEmpty(message = "Last name is required")
+    @NotBlank(message = "Last name is required")
+    private String lastname;
+
+    @Email(message = "Email should be valid -> sajan@email.com")
+    @NotEmpty(message = "Email is required")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotEmpty(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password should be at least 6 characters long")
+    private String password;
+}
