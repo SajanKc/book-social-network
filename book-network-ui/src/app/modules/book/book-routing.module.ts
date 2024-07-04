5483af5cc5706d1da10fdd0c2a4e945469d8bd4d
sajan96kc <sajan1996kc@gmail.com>
2024-07-04 21:32:16 +0545

    add: book card, list, rating, main component and http interceptor

--- book-network-ui/src/app/modules/book/book-routing.module.ts
+++ book-network-ui/src/app/modules/book/book-routing.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {MenuComponent} from "./pages/components/menu/menu.component";
+import {MainComponent} from "./pages/main/main.component";
+import {BookListComponent} from "./pages/components/book-list/book-list.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: MenuComponent
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: BookListComponent,
+      }
+    ]
   }
 ];
 
--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -3,10 +3,18 @@ import {CommonModule} from '@angular/common';
 
 import {BookRoutingModule} from './book-routing.module';
 import {MenuComponent} from './pages/components/menu/menu.component';
+import { BookListComponent } from './pages/components/book-list/book-list.component';
+import { MainComponent } from './pages/main/main.component';
+import { BookCardComponent } from './pages/components/book-card/book-card.component';
+import { RatingComponent } from './pages/components/rating/rating.component';
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    BookListComponent,
+    MainComponent,
+    BookCardComponent,
+    RatingComponent
   ],
   imports: [
     CommonModule,
